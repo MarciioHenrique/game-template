2fbb0eac8fac98f47dbcf03cc833f34f6ae5e43f
Marcio Henrique <dev.marcioh@gmail.com>
2024-09-14 20:13:58 -0300

    feat: configurations base page

--- src/app/configurations/page.tsx
+++ src/app/configurations/page.tsx
@@ -1,7 +1,78 @@
 "use client";
 
-import { Layout, Typography } from "antd";
+import { Radio, Input, Row, Col, Layout, Form } from "antd";
 
 export default function Configurations() {
-  return <div>Configurations</div>;
+  return (
+    <Layout.Content className="w-full h-full p-8">
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 24 }}>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <Form.Item
+            name="Nome do Projeto"
+            label="Nome do Projeto"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+            labelCol={{ span: 24 }}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <Form.Item
+            name="Dimensões da Tela"
+            label="Dimensões da Tela"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+            labelCol={{ span: 24 }}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 24 }}>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <Form.Item
+            name="Proporções da Tela"
+            label="Proporções da Tela"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+            labelCol={{ span: 24 }}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={24} md={12} lg={12}>
+          <Form.Item
+            name="Git Ignore"
+            label="Git Ignore"
+            rules={[
+              {
+                required: true,
+                message: "Por favor, selecione uma opção.",
+              },
+            ]}
+            labelCol={{ span: 24 }}
+          >
+            <Radio.Group>
+              <Radio key="Sim" value={false}>
+                Sim
+              </Radio>
+              <Radio key="Não" value={true}>
+                Não
+              </Radio>
+            </Radio.Group>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Layout.Content>
+  );
 }
