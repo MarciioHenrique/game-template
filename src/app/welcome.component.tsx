7d3bab5bbac1f096e9274f34451a79e20fa298a4
Marcio Henrique <dev.marcioh@gmail.com>
2024-09-14 16:57:38 -0300

    refactor: base welcome page

--- src/app/welcome.component.tsx
+++ src/app/welcome.component.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { routes } from "@/routes/routes";
-import { Layout, Button } from "antd";
+import { Layout, Button, Typography } from "antd";
 
 export default function Welcome() {
   return (
     <Layout.Content className="w-full h-full flex flex-col justify-center items-center">
-      <h1>Welcome</h1>
-      <p>This is a game template.</p>
-      <Button type="primary" href={routes.configurations}>
+      <Typography.Title> Welcome to Game Template </Typography.Title>
+      <Typography.Text>
+        This is an game template develop to help on game production. You can
+        start clicking on the button below.
+      </Typography.Text>
+      <Button type="primary" href={routes.configurations} className="m-8">
         Start
       </Button>
     </Layout.Content>
